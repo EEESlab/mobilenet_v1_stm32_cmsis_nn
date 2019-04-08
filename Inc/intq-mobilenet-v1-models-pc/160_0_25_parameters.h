6e1546a87c71134dd995dade60f5938229f133b6
Alessandro Capotondi <alessandro.capotondi@unibo.it>
2019-04-08 11:25:58 +0200

    Mixed-Precision MobilenetV1 v1.0.0
     ## Brief
     Mixed-Precision MobilenetV1 template.
    
     ## Added
     Mobilenet Models: 'Inc/intq-mobilenet-v1-models-pc',
     'Inc/intq-mobilenet-v1-models'
    
     ## Changed
     'main.c': now based on generated defines for automatic deployment of
     different MobilnetV1 models.

---
+++ Inc/intq-mobilenet-v1-models-pc/160_0_25_parameters.h
@@ -0,0 +1,361 @@
+#ifndef __160_0_25_PARAMETERS_H__
+#define __160_0_25_PARAMETERS_H__
+
+
+/* Layer 1 Topology Parameters */
+#define CONV1_IM_DIM    	(160)
+#define CONV1_IM_CH     	(4)
+#define CONV1_KER_DIM   	(3)
+#define CONV1_L_PADDING 	(0)
+#define CONV1_R_PADDING 	(1)
+#define CONV1_T_PADDING 	(0)
+#define CONV1_B_PADDING 	(1)
+#define CONV1_STRIDE    	(2)
+#define CONV1_OUT_CH    	(8)
+#define CONV1_OUT_DIM    	(80)
+
+
+/* Layer 2 Topology Parameters */
+#define CONV2_IM_DIM    	(80)
+#define CONV2_IM_CH     	(8)
+#define CONV2_KER_DIM   	(3)
+#define CONV2_L_PADDING 	(1)
+#define CONV2_R_PADDING 	(1)
+#define CONV2_T_PADDING 	(1)
+#define CONV2_B_PADDING 	(1)
+#define CONV2_STRIDE    	(1)
+#define CONV2_OUT_CH    	(8)
+#define CONV2_OUT_DIM    	(80)
+
+
+/* Layer 3 Topology Parameters */
+#define CONV3_IM_DIM    	(80)
+#define CONV3_IM_CH     	(8)
+#define CONV3_KER_DIM   	(1)
+#define CONV3_L_PADDING 	(0)
+#define CONV3_R_PADDING 	(0)
+#define CONV3_T_PADDING 	(0)
+#define CONV3_B_PADDING 	(0)
+#define CONV3_STRIDE    	(1)
+#define CONV3_OUT_CH    	(16)
+#define CONV3_OUT_DIM    	(80)
+
+
+/* Layer 4 Topology Parameters */
+#define CONV4_IM_DIM    	(80)
+#define CONV4_IM_CH     	(16)
+#define CONV4_KER_DIM   	(3)
+#define CONV4_L_PADDING 	(0)
+#define CONV4_R_PADDING 	(1)
+#define CONV4_T_PADDING 	(0)
+#define CONV4_B_PADDING 	(1)
+#define CONV4_STRIDE    	(2)
+#define CONV4_OUT_CH    	(16)
+#define CONV4_OUT_DIM    	(40)
+
+
+/* Layer 5 Topology Parameters */
+#define CONV5_IM_DIM    	(40)
+#define CONV5_IM_CH     	(16)
+#define CONV5_KER_DIM   	(1)
+#define CONV5_L_PADDING 	(0)
+#define CONV5_R_PADDING 	(0)
+#define CONV5_T_PADDING 	(0)
+#define CONV5_B_PADDING 	(0)
+#define CONV5_STRIDE    	(1)
+#define CONV5_OUT_CH    	(32)
+#define CONV5_OUT_DIM    	(40)
+
+
+/* Layer 6 Topology Parameters */
+#define CONV6_IM_DIM    	(40)
+#define CONV6_IM_CH     	(32)
+#define CONV6_KER_DIM   	(3)
+#define CONV6_L_PADDING 	(1)
+#define CONV6_R_PADDING 	(1)
+#define CONV6_T_PADDING 	(1)
+#define CONV6_B_PADDING 	(1)
+#define CONV6_STRIDE    	(1)
+#define CONV6_OUT_CH    	(32)
+#define CONV6_OUT_DIM    	(40)
+
+
+/* Layer 7 Topology Parameters */
+#define CONV7_IM_DIM    	(40)
+#define CONV7_IM_CH     	(32)
+#define CONV7_KER_DIM   	(1)
+#define CONV7_L_PADDING 	(0)
+#define CONV7_R_PADDING 	(0)
+#define CONV7_T_PADDING 	(0)
+#define CONV7_B_PADDING 	(0)
+#define CONV7_STRIDE    	(1)
+#define CONV7_OUT_CH    	(32)
+#define CONV7_OUT_DIM    	(40)
+
+
+/* Layer 8 Topology Parameters */
+#define CONV8_IM_DIM    	(40)
+#define CONV8_IM_CH     	(32)
+#define CONV8_KER_DIM   	(3)
+#define CONV8_L_PADDING 	(0)
+#define CONV8_R_PADDING 	(1)
+#define CONV8_T_PADDING 	(0)
+#define CONV8_B_PADDING 	(1)
+#define CONV8_STRIDE    	(2)
+#define CONV8_OUT_CH    	(32)
+#define CONV8_OUT_DIM    	(20)
+
+
+/* Layer 9 Topology Parameters */
+#define CONV9_IM_DIM    	(20)
+#define CONV9_IM_CH     	(32)
+#define CONV9_KER_DIM   	(1)
+#define CONV9_L_PADDING 	(0)
+#define CONV9_R_PADDING 	(0)
+#define CONV9_T_PADDING 	(0)
+#define CONV9_B_PADDING 	(0)
+#define CONV9_STRIDE    	(1)
+#define CONV9_OUT_CH    	(64)
+#define CONV9_OUT_DIM    	(20)
+
+
+/* Layer 10 Topology Parameters */
+#define CONV10_IM_DIM    	(20)
+#define CONV10_IM_CH     	(64)
+#define CONV10_KER_DIM   	(3)
+#define CONV10_L_PADDING 	(1)
+#define CONV10_R_PADDING 	(1)
+#define CONV10_T_PADDING 	(1)
+#define CONV10_B_PADDING 	(1)
+#define CONV10_STRIDE    	(1)
+#define CONV10_OUT_CH    	(64)
+#define CONV10_OUT_DIM    	(20)
+
+
+/* Layer 11 Topology Parameters */
+#define CONV11_IM_DIM    	(20)
+#define CONV11_IM_CH     	(64)
+#define CONV11_KER_DIM   	(1)
+#define CONV11_L_PADDING 	(0)
+#define CONV11_R_PADDING 	(0)
+#define CONV11_T_PADDING 	(0)
+#define CONV11_B_PADDING 	(0)
+#define CONV11_STRIDE    	(1)
+#define CONV11_OUT_CH    	(64)
+#define CONV11_OUT_DIM    	(20)
+
+
+/* Layer 12 Topology Parameters */
+#define CONV12_IM_DIM    	(20)
+#define CONV12_IM_CH     	(64)
+#define CONV12_KER_DIM   	(3)
+#define CONV12_L_PADDING 	(0)
+#define CONV12_R_PADDING 	(1)
+#define CONV12_T_PADDING 	(0)
+#define CONV12_B_PADDING 	(1)
+#define CONV12_STRIDE    	(2)
+#define CONV12_OUT_CH    	(64)
+#define CONV12_OUT_DIM    	(10)
+
+
+/* Layer 13 Topology Parameters */
+#define CONV13_IM_DIM    	(10)
+#define CONV13_IM_CH     	(64)
+#define CONV13_KER_DIM   	(1)
+#define CONV13_L_PADDING 	(0)
+#define CONV13_R_PADDING 	(0)
+#define CONV13_T_PADDING 	(0)
+#define CONV13_B_PADDING 	(0)
+#define CONV13_STRIDE    	(1)
+#define CONV13_OUT_CH    	(128)
+#define CONV13_OUT_DIM    	(10)
+
+
+/* Layer 14 Topology Parameters */
+#define CONV14_IM_DIM    	(10)
+#define CONV14_IM_CH     	(128)
+#define CONV14_KER_DIM   	(3)
+#define CONV14_L_PADDING 	(1)
+#define CONV14_R_PADDING 	(1)
+#define CONV14_T_PADDING 	(1)
+#define CONV14_B_PADDING 	(1)
+#define CONV14_STRIDE    	(1)
+#define CONV14_OUT_CH    	(128)
+#define CONV14_OUT_DIM    	(10)
+
+
+/* Layer 15 Topology Parameters */
+#define CONV15_IM_DIM    	(10)
+#define CONV15_IM_CH     	(128)
+#define CONV15_KER_DIM   	(1)
+#define CONV15_L_PADDING 	(0)
+#define CONV15_R_PADDING 	(0)
+#define CONV15_T_PADDING 	(0)
+#define CONV15_B_PADDING 	(0)
+#define CONV15_STRIDE    	(1)
+#define CONV15_OUT_CH    	(128)
+#define CONV15_OUT_DIM    	(10)
+
+
+/* Layer 16 Topology Parameters */
+#define CONV16_IM_DIM    	(10)
+#define CONV16_IM_CH     	(128)
+#define CONV16_KER_DIM   	(3)
+#define CONV16_L_PADDING 	(1)
+#define CONV16_R_PADDING 	(1)
+#define CONV16_T_PADDING 	(1)
+#define CONV16_B_PADDING 	(1)
+#define CONV16_STRIDE    	(1)
+#define CONV16_OUT_CH    	(128)
+#define CONV16_OUT_DIM    	(10)
+
+
+/* Layer 17 Topology Parameters */
+#define CONV17_IM_DIM    	(10)
+#define CONV17_IM_CH     	(128)
+#define CONV17_KER_DIM   	(1)
+#define CONV17_L_PADDING 	(0)
+#define CONV17_R_PADDING 	(0)
+#define CONV17_T_PADDING 	(0)
+#define CONV17_B_PADDING 	(0)
+#define CONV17_STRIDE    	(1)
+#define CONV17_OUT_CH    	(128)
+#define CONV17_OUT_DIM    	(10)
+
+
+/* Layer 18 Topology Parameters */
+#define CONV18_IM_DIM    	(10)
+#define CONV18_IM_CH     	(128)
+#define CONV18_KER_DIM   	(3)
+#define CONV18_L_PADDING 	(1)
+#define CONV18_R_PADDING 	(1)
+#define CONV18_T_PADDING 	(1)
+#define CONV18_B_PADDING 	(1)
+#define CONV18_STRIDE    	(1)
+#define CONV18_OUT_CH    	(128)
+#define CONV18_OUT_DIM    	(10)
+
+
+/* Layer 19 Topology Parameters */
+#define CONV19_IM_DIM    	(10)
+#define CONV19_IM_CH     	(128)
+#define CONV19_KER_DIM   	(1)
+#define CONV19_L_PADDING 	(0)
+#define CONV19_R_PADDING 	(0)
+#define CONV19_T_PADDING 	(0)
+#define CONV19_B_PADDING 	(0)
+#define CONV19_STRIDE    	(1)
+#define CONV19_OUT_CH    	(128)
+#define CONV19_OUT_DIM    	(10)
+
+
+/* Layer 20 Topology Parameters */
+#define CONV20_IM_DIM    	(10)
+#define CONV20_IM_CH     	(128)
+#define CONV20_KER_DIM   	(3)
+#define CONV20_L_PADDING 	(1)
+#define CONV20_R_PADDING 	(1)
+#define CONV20_T_PADDING 	(1)
+#define CONV20_B_PADDING 	(1)
+#define CONV20_STRIDE    	(1)
+#define CONV20_OUT_CH    	(128)
+#define CONV20_OUT_DIM    	(10)
+
+
+/* Layer 21 Topology Parameters */
+#define CONV21_IM_DIM    	(10)
+#define CONV21_IM_CH     	(128)
+#define CONV21_KER_DIM   	(1)
+#define CONV21_L_PADDING 	(0)
+#define CONV21_R_PADDING 	(0)
+#define CONV21_T_PADDING 	(0)
+#define CONV21_B_PADDING 	(0)
+#define CONV21_STRIDE    	(1)
+#define CONV21_OUT_CH    	(128)
+#define CONV21_OUT_DIM    	(10)
+
+
+/* Layer 22 Topology Parameters */
+#define CONV22_IM_DIM    	(10)
+#define CONV22_IM_CH     	(128)
+#define CONV22_KER_DIM   	(3)
+#define CONV22_L_PADDING 	(1)
+#define CONV22_R_PADDING 	(1)
+#define CONV22_T_PADDING 	(1)
+#define CONV22_B_PADDING 	(1)
+#define CONV22_STRIDE    	(1)
+#define CONV22_OUT_CH    	(128)
+#define CONV22_OUT_DIM    	(10)
+
+
+/* Layer 23 Topology Parameters */
+#define CONV23_IM_DIM    	(10)
+#define CONV23_IM_CH     	(128)
+#define CONV23_KER_DIM   	(1)
+#define CONV23_L_PADDING 	(0)
+#define CONV23_R_PADDING 	(0)
+#define CONV23_T_PADDING 	(0)
+#define CONV23_B_PADDING 	(0)
+#define CONV23_STRIDE    	(1)
+#define CONV23_OUT_CH    	(128)
+#define CONV23_OUT_DIM    	(10)
+
+
+/* Layer 24 Topology Parameters */
+#define CONV24_IM_DIM    	(10)
+#define CONV24_IM_CH     	(128)
+#define CONV24_KER_DIM   	(3)
+#define CONV24_L_PADDING 	(0)
+#define CONV24_R_PADDING 	(1)
+#define CONV24_T_PADDING 	(0)
+#define CONV24_B_PADDING 	(1)
+#define CONV24_STRIDE    	(2)
+#define CONV24_OUT_CH    	(128)
+#define CONV24_OUT_DIM    	(5)
+
+
+/* Layer 25 Topology Parameters */
+#define CONV25_IM_DIM    	(5)
+#define CONV25_IM_CH     	(128)
+#define CONV25_KER_DIM   	(1)
+#define CONV25_L_PADDING 	(0)
+#define CONV25_R_PADDING 	(0)
+#define CONV25_T_PADDING 	(0)
+#define CONV25_B_PADDING 	(0)
+#define CONV25_STRIDE    	(1)
+#define CONV25_OUT_CH    	(256)
+#define CONV25_OUT_DIM    	(5)
+
+
+/* Layer 26 Topology Parameters */
+#define CONV26_IM_DIM    	(5)
+#define CONV26_IM_CH     	(256)
+#define CONV26_KER_DIM   	(3)
+#define CONV26_L_PADDING 	(1)
+#define CONV26_R_PADDING 	(1)
+#define CONV26_T_PADDING 	(1)
+#define CONV26_B_PADDING 	(1)
+#define CONV26_STRIDE    	(1)
+#define CONV26_OUT_CH    	(256)
+#define CONV26_OUT_DIM    	(5)
+
+
+/* Layer 27 Topology Parameters */
+#define CONV27_IM_DIM    	(5)
+#define CONV27_IM_CH     	(256)
+#define CONV27_KER_DIM   	(1)
+#define CONV27_L_PADDING 	(0)
+#define CONV27_R_PADDING 	(0)
+#define CONV27_T_PADDING 	(0)
+#define CONV27_B_PADDING 	(0)
+#define CONV27_STRIDE    	(1)
+#define CONV27_OUT_CH    	(256)
+#define CONV27_OUT_DIM    	(5)
+
+
+/* Layer 28 Topology Parameters */
+#define FC28_IM_CH     	(256)
+#define FC28_OUT_CH    	(1000)
+
+
+#endif /*__160_0_25_PARAMETERS_H__*/
